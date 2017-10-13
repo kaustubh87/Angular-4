fd0a344cb1badfd012d467d6ab91735cd0cc8080
kvin1087 <kaustubh87@gmail.com>
2017-10-13 15:31:34 -0500

    Added methods to the class

--- typescript/classes.ts
+++ typescript/classes.ts
@@ -7,5 +7,28 @@ class Car{
         this.speed = speed || 0;
     }
 
-    
+    accelerate(): void {
+        this.speed++;
+    }
+
+    throttle():void {
+        this.speed--;
+    }
+
+    getSpeed(): void{
+        console.log(this.speed);
+    }
+
+    static numberOfWheels(): number{
+        return 4;
+    }
+
+
 }
+
+
+// Instantiate (create) an object from a class
+
+let car = new Car(5);
+car.accelerate();
+car.getSpeed();
