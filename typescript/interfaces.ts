032b044cb2d3d8fcf4d49645bd82dd708b76655e
kvin1087 <kaustubh87@gmail.com>
2017-10-14 18:48:34 -0500

    Interfaces in typescript

---
+++ typescript/interfaces.ts
@@ -0,0 +1,9 @@
+interface User{
+    username: string;
+    password: string;
+    confirmPassword?: string; // Optional property 
+}
+
+let user: User;
+
+user = {username: 'max', password: 'supersecret' };
