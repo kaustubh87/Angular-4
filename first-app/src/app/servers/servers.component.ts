219bc8fd71040623155f7b3ef9b45c0318b05459
kvin1087 <kaustubh87@gmail.com>
2017-10-18 15:40:13 -0500

    Property Binding and Event Binding Done

--- first-app/src/app/servers/servers.component.ts
+++ first-app/src/app/servers/servers.component.ts
@@ -19,7 +19,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = "No server was created";
-  serverName = '';
+  serverName = 'Test Server';
 
   constructor() {
     setTimeout(() => {
@@ -31,7 +31,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! Name is ' +this.serverName;
   }
 
   onUpdateServerName(event: any){
