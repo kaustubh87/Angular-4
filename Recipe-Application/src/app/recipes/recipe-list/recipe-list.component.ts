08e3253ba187cdf1b933e23cdba41c1fdd30c69b
kvin1087 <kaustubh87@gmail.com>
2017-10-28 18:04:08 -0500

    Formatting list item

--- Recipe-Application/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Recipe-Application/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,9 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiopoOtspTXAhXl3YMKHVZaBOUQjRwIBw&url=http%3A%2F%2Fmaxpixel.freegreatpicture.com%2FBio-Food-Home-Made-Recipe-Dishes-Kitchen-Meals-1175496&psig=AOvVaw2k2J4bb4Gk4SPDQPoko_jA&ust=1509317057981639')
+    new Recipe('A Test Recipe',
+    'This is a test',
+    'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiopoOtspTXAhXl3YMKHVZaBOUQjRwIBw&url=http%3A%2F%2Fmaxpixel.freegreatpicture.com%2FBio-Food-Home-Made-Recipe-Dishes-Kitchen-Meals-1175496&psig=AOvVaw2k2J4bb4Gk4SPDQPoko_jA&ust=1509317057981639')
   ];
 
   constructor() { }
