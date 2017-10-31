3d3f17fa6e1113e19f897e34d93eee5882027a97
kvin1087 <kaustubh87@gmail.com>
2017-10-31 09:48:28 -0500

    Moved template

--- Components-Databinding/src/app/app.component.ts
+++ Components-Databinding/src/app/app.component.ts
@@ -6,8 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  serverElements = [{ type: 'server', name: 'TestServer', content: 'Just a test!'}];
+
 
 }
