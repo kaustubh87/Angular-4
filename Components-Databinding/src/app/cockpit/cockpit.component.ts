f9e2f790789acefa1e8ae4a6d2488cb9b19e7739
kvin1087 <kaustubh87@gmail.com>
2017-10-31 09:48:19 -0500

    Added properties

--- Components-Databinding/src/app/cockpit/cockpit.component.ts
+++ Components-Databinding/src/app/cockpit/cockpit.component.ts
@@ -7,25 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
 
+  newServerName = '';
+  newServerContent = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
   onAddServer() {
-    this.serverElements.push({
+   /* this.serverElements.push({
       type: 'server',
       name: this.newServerName,
       content: this.newServerContent
     });
+    */
   }
 
   onAddBlueprint() {
-    this.serverElements.push({
+   /*this.serverElements.push({
       type: 'blueprint',
       name: this.newServerName,
       content: this.newServerContent
     });
+    */
   }
 
 }
