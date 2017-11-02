77b112f26661165a19fe140480b2087b5075fd22
kvin1087 <kaustubh87@gmail.com>
2017-11-01 21:23:45 -0500

    Formatting code

--- Components-Databinding/src/app/app.component.ts
+++ Components-Databinding/src/app/app.component.ts
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{ type: 'server', name: 'TestServer', content: 'Just a test!'}];
+  serverElements =
+  [
+    {
+      type: 'server',
+      name: 'TestServer',
+      content: 'Just a test!'
+    }
+  ];
 
 
 }
