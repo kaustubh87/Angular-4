e0eabb14781f0eef9f9f5be695da102d3c949946
kvin1087 <kaustubh87@gmail.com>
2017-11-03 11:49:22 -0500

    Binding through alias

--- Components-Databinding/src/app/server-element/server-element.component.ts
+++ Components-Databinding/src/app/server-element/server-element.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
-  @Input() element: {
+  // tslint:disable-next-line:no-input-rename
+  @Input('srvElement') element: {
     type: string,
     name: string,
     content: string
